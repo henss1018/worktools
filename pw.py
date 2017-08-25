535a50ad7e47a1d19afcbdebc6ad662673fa6ee1
henss1018 <henss@139.com>
2017-08-25 11:03:48 +0800

    增加素材文件

---
+++ pw.py
@@ -0,0 +1,20 @@
+#! python3
+# pw.py - An insecure password locker program.
+
+PASSWORDS = {'email':'abc',
+             'blog':'cde',
+             'ioffice':'ggg',
+             'weibo':'ttt',
+             'qq':'hhh'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+    print ('Usage: python pw.py [account] - copy account password')
+    sys.exit()
+account = sys.argv[1]
+
+if account in PASSWORDS:
+    pyperclip.copy(PASSWORDS[account])
+    print ('Password for ' + account + ' copied to clipboard.')
+else:
+    print ('There is no account named ' + account)
