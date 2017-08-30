eb18d1a4ad012671749f1f02f0d7bd71959ed850
henss1018 <henss@139.com>
2017-08-30 17:26:06 +0800

    update brosertool

--- brosertool.py
+++ brosertool.py
@@ -1,4 +1,5 @@
 #! python3
+# coding=utf-8
 
 from selenium import webdriver
 import time
@@ -24,61 +25,65 @@ def brosertest():
 
 def mailTo(in_addr, in_content):
     browser = webdriver.Chrome()
-    browser.implicitly_wait(30)
+    #browser.implicitly_wait(5)
+    #打开主页
     browser.get('http://mail.10086.cn')
     browser.maximize_window()
     #time.sleep(5)
+    #获取用户名
     linkElemUser = browser.find_element_by_id('txtUser')
     linkElemUser.clear()
     linkElemUser.send_keys('henss')
+    #获取密码
     linkElemPwd = browser.find_element_by_id('txtPass')
-    linkElemPwd.send_keys('huangshi1982')
+    linkElemPwd.send_keys('***')
+    #获取登录按钮
     linkElemSubmit = browser.find_element_by_id('loginBtn')
     linkElemSubmit.click()
     #time.sleep(5)
+    #获取写信按钮
     linkElemWrite = browser.find_element_by_id('btn_compose')
     linkElemWrite.click()
     #time.sleep(2)
     #browser.implicitly_wait(2)
-
+    #获取所有iframe
     iframes = browser.find_elements_by_tag_name('iframe')
     print(len(iframes))
     for i in iframes:
         print(i.get_attribute('class') +'  ' + i.get_attribute('name')  + '  ' +i.get_attribute('id'))
+    #切换到内容iframe
     browser.switch_to.frame(iframes[2])
-    #time.sleep(2)
-
-    linkElemAddr = browser.find_element_by_class_name('addrText-input')
-    linkElemAddr.send_keys('henss@139.com')
-  #  time.sleep(5)
-    locator = (By.ID, 'txtSubject')
-    try:
-        WebDriverWait(browser, 10, 0.5).until(EC.presence_of_element_located(locator))
-        linkElemSubj = browser.find_element_by_id('txtSubject')
-      #  time.sleep(5)
-        linkElemSubj.send_keys('setestmail')
-      #  time.sleep(5)
-    finally:
-        print('done')
-   # time.sleep(6)
+    #获取内容iframe的所有子iframe
     subiframes = browser.find_elements_by_tag_name('iframe')
     print(len(subiframes))
     for i in subiframes:
-        print(i.get_attribute('class') +'  ' + i.get_attribute('name'))
-   # print(subiframes[1].get_attribute('tabindex'))
+        print(i.get_attribute('class') + '  ' + i.get_attribute('name'))
+        # print(subiframes[1].get_attribute('tabindex'))
+    #切换到编辑器的iframe，并添加内容
     browser.switch_to.frame(subiframes[1])
- #   time.sleep(5)
-   # time.sleep(6)
-
-  #  browser.switch_to.frame(browser.find_element_by_xpath("//*[@id='view_0.775335838540318']/div[3]/div[2]/iframe"))
     linkElemCont = browser.find_element_by_tag_name('body')
- #   time.sleep(5)
-    linkElemCont.send_keys('测试')
- #   time.sleep(5)
+    linkElemCont.send_keys('测试邮件，自动发送')
+    #   time.sleep(5)
+    #切回内容iframe
     browser.switch_to.default_content()
- #   time.sleep(5)
     browser.switch_to.frame(iframes[2])
- #   time.sleep(5)
+    #获取主题
+    linkElemSubj = browser.find_element_by_id('txtSubject')
+    linkElemSubj.send_keys('setestmail')
+    #获取收件人地址控件
+    linkElemAddr = browser.find_element_by_class_name('addrText-input')
+    linkElemAddr.send_keys('henss@139.com')
+  #  time.sleep(5)
+  #  同步等待加载成功后再执行
+  #  locator = (By.ID, 'txtSubject')
+  #  try:
+    #WebDriverWait(browser, 10, 0.5).until(EC.presence_of_element_located(locator))
+
+      #  time.sleep(5)
+   # finally:
+   #     print('done')
+   # time.sleep(6)
+    #获取发送信件按钮
     linkElemSend = browser.find_element_by_id('topSend')
     time.sleep(2)
     linkElemSend.click()
